cf468ba9e4a2063ead8fd376990b48d98218c755
Noah Isaacson <noah@nisaacson.com>
2013-04-15 17:46:39 -0400

    use http-proxy instead of request

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 var http = require('http'),
-    request = require('request');
+    httpProxy = require('http-proxy');
 var inspect = require('eyespect').inspector();
 module.exports = function (listenPort, forwardPort) {
-  var server = http.createServer(function(req,res) {
-    var url = req.url
-    var forwardURL = 'http://localhost:' + forwardPort + url
-    req.pipe(request(forwardURL)).pipe(res)
-  })
-  server.listen(listenPort)
+  var server = httpProxy.createServer(forwardPort, 'localhost').listen(listenPort);
   inspect(listenPort, 'Proxy server listening on port')
   inspect(forwardPort, 'Proxy server forwarding to  port')
   return server
